a6456955525c1bf2e7929d0c2e949086a11fdd6a
spejman <spejman@e12d007a-0d59-46f4-9d14-2c51302ae902>
2006-12-21 22:54:18 +0000

    * to_mp3  * language support for to_speech
    git-svn-id: svn+ssh://rubyforge.org/var/svn/festivaltts4r/HEAD@9 e12d007a-0d59-46f4-9d14-2c51302ae902

--- festivaltts4r/lib/festivaltts4r/festival4r.rb
+++ festivaltts4r/lib/festivaltts4r/festival4r.rb
@@ -3,7 +3,18 @@
 # 
 
 class String
-  def to_speech(text=self)
-    system("echo \"#{text.to_s}\" | festival --tts")
+  
+  def to_speech(params={})
+  
+    text = params[:text] || self
+    language = "--language " + params[:language] if params[:language]
+    system("echo \"#{text.to_s}\" | festival --tts #{language}")
   end
+  
+  def to_mp3(filename, params={})
+
+    text = params[:text] || self
+    system("echo \"#{text.to_s}\" | text2wave #{language} | lame - - > #{filename}.mp3 2> /dev/null")
+  end
+  
 end
