976b99cc027d81732c43f536080475104e7f533f
spejman <spejman@e12d007a-0d59-46f4-9d14-2c51302ae902>
2007-04-19 11:17:38 +0000

    * plugin install script.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/festivaltts4r/HEAD@12 e12d007a-0d59-46f4-9d14-2c51302ae902

--- festivaltts_on_rails/install.rb
+++ festivaltts_on_rails/install.rb
@@ -1,1 +1,14 @@
-# Install hook code here
+#RAILS_ROOT="../../../" unless defined?(RAILS_ROOT)
+require "fileutils"
+require File.join("#{File.dirname(__FILE__)}/lib/", "festivaltts_on_rails.rb")
+
+def copy_files(source_path, destination_path, directory)
+  source, destination = File.join(directory, source_path), File.join(RAILS_ROOT, destination_path)
+  FileUtils.mkdir(destination) unless File.exist?(destination)
+  FileUtils.cp_r(Dir.glob(source+'/*.*'), destination)
+end
+
+directory = File.dirname(__FILE__)
+# Subdir for tmp mp3 files
+FileUtils.mkdir(MP3_FOLDER_PATH)
+copy_files("/flash", "/public/flash", directory)
