1fa15208f17fdb2b1035011c024fed0a71d5ec48
spejman <spejman@e12d007a-0d59-46f4-9d14-2c51302ae902>
2007-09-13 09:01:48 +0000

    * remove redundant options
    git-svn-id: svn+ssh://rubyforge.org/var/svn/festivaltts4r/HEAD@22 e12d007a-0d59-46f4-9d14-2c51302ae902

--- festivaltts_on_rails/lib/festivaltts_on_rails.rb
+++ festivaltts_on_rails/lib/festivaltts_on_rails.rb
@@ -13,22 +13,24 @@ MP3_FOLDER_PATH = "#{RAILS_ROOT}/public/#{MP3_FOLDER_URL}"
 # - width: default = 200 
 # - height: default = 20
 def text_to_flash_player(text, opts = {})
-  bgcolor = opts[:bgcolor] if opts[:bgcolor]
-  width = opts[:width] if opts[:width]
-  height = opts[:height] if opts[:height]
-
+  bgcolor = (opts[:bgcolor] if opts[:bgcolor]) || "FFFFFF"
+  width = (opts[:width] if opts[:width]) || 200
+  height = (opts[:height] if opts[:height]) || 20
+  autoplay = opts[:autoplay]
+  
   filename =  Digest::MD5.new(text).to_s + ".mp3"
-    
+  
   text.to_mp3(MP3_FOLDER_PATH + "/" + filename) unless File.exists?(MP3_FOLDER_PATH + "/" + filename)  
-  html_for_mp3_flash(MP3_FOLDER_URL + "/" + filename)
+  html_for_mp3_flash(MP3_FOLDER_URL + "/" + filename, bgcolor, width, height, autoplay)
 
 end
 
 # Returns needed html for playing mp3.
-def html_for_mp3_flash(filename, bgcolor = "FFFFFF", width = 200, height = 20)
+def html_for_mp3_flash(filename, bgcolor = "FFFFFF", width = 200, height = 20, autoplay = false)
+  autoplay = autoplay ? 1 : 0
   "    <object type=\"application/x-shockwave-flash\"\n \
-   data=\"#{MP3_FLASH_PLAYER_URL}?son=#{filename}&amp;bgcolor=#{bgcolor}\" width=\"#{width}\"\n \
+   data=\"#{MP3_FLASH_PLAYER_URL}?son=#{filename}&amp;bgcolor=#{bgcolor}&amp;autoplay=#{autoplay}\" width=\"#{width}\"\n \
    height=\"#{height}\">\n \
-   <param name=\"movie\" value=\"#{MP3_FLASH_PLAYER_URL}?son=#{filename}&amp;bgcolor=#{bgcolor}\" />\n \
+   <param name=\"movie\" value=\"#{MP3_FLASH_PLAYER_URL}?son=#{filename}&amp;bgcolor=#{bgcolor}&amp;autoplay=#{autoplay}\" />\n \
    </object>"
 end
