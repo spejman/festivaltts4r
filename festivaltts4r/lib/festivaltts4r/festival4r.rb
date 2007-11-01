884bc9b0ec29c8bfed9d775b08f0577ab8199b39
mikemondragon <mikemondragon@e12d007a-0d59-46f4-9d14-2c51302ae902>
2007-11-01 01:36:20 +0000

    Adding FestivalError and FestivalSystemError for custom exception raising. to_speech uses IO.popen and raises exceptions now.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/festivaltts4r/HEAD@23 e12d007a-0d59-46f4-9d14-2c51302ae902

--- festivaltts4r/lib/festivaltts4r/festival4r.rb
+++ festivaltts4r/lib/festivaltts4r/festival4r.rb
@@ -16,13 +16,29 @@
 class String
   
   # Outputs the speech generated with festival tts and the string itself.
-  # Can handle two options:
+  # Can handle these options:
   # - text --> speech given text instead of the string itself.
   # - language --> speech language desired (festival voices for given languages are required )  
+  # - festival --> alternative festival program and options besides 'festival --tts'
+
   def to_speech(params={})  
     text = params[:text] || self
+    festival = params[:festival] || "festival --tts"
     language = "--language " + params[:language] if params[:language]
-    system("echo \"#{text.to_s}\" | festival --tts #{language}")
+
+    cmd = "echo \"#{text.to_s}\" | #{festival} #{language}"
+
+    begin
+      out = IO.popen(cmd)
+      Process.wait
+      e = $?.exitstatus
+    rescue StandardError => err
+      raise FestivalError.new(err)
+    else
+      raise FestivalSystemError.new("0 not returned:\n#{cmd}\n#{out.readlines}"
+        ) unless e.eql?(0)
+    end
+
   end
   
   # Creates a file with name "filename" and with the generated with festival tts, the string itself and lame.
@@ -35,3 +51,27 @@ class String
   end
   
 end
+
+##
+# A generic application error wrapper for festivaltts4r
+
+class FestivalError < RuntimeError
+
+  ##
+  # The original exception
+
+  attr_accessor :original_exception
+
+  ##
+  # Creates a new MediaBotError with +message+ and +original_exception+
+
+  def initialize(e)
+    exception = e.class == String ? StandardError.new(e) : e
+    @original_exception = exception
+    message = "festivaltts4r error: #{exception.message} (#{exception.class})"
+    super message
+  end
+
+end
+
+class FestivalSystemError < FestivalError; end
--- festivaltts4r/test/festivaltts4r_test.rb
+++ festivaltts4r/test/festivaltts4r_test.rb
@@ -2,10 +2,16 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class Festivaltts4rTest < Test::Unit::TestCase
 
-  def setup
+  def test_to_speech_raises_festival_error
+    assert_raises(FestivalSystemError) do
+      params = {:festival => '/bin/not-a-real-program'}
+      "hello world".to_speech(params)
+    end
+
+    assert_raises(FestivalSystemError) do
+      params = {:festival => '/bin/false'}
+      "hello world".to_speech(params)
+    end
   end
   
-  def test_truth
-    assert true
-  end
 end
